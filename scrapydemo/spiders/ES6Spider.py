affa1da64666b71db2c3a0adc074454fcc7823f8
wuketao <wuketao@houbank.com>
2019-05-01 13:00:37 +0800

    init

---
+++ scrapydemo/spiders/ES6Spider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class ES6Spider(scrapy.Spider):
+    name = 'ES6'
+    allowed_domains = ['es6.ruanyifeng.com']
+    start_urls = ['http://es6.ruanyifeng.com/']
+
+    def parse(self, response):
+        print(response)
