8bdaf04fadcb2f79cc159f178e891f8dcb095b47
wuketao <wuketao@houbank.com>
2019-05-15 18:35:56 +0800

    learn

--- scrapydemo/items.py
+++ scrapydemo/items.py
@@ -10,5 +10,6 @@ import scrapy
 
 class ScrapydemoItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
+    content = scrapy.Field()
     pass
--- scrapydemo/spiders/ES6Spider.py
+++ scrapydemo/spiders/ES6Spider.py
@@ -4,7 +4,7 @@ import scrapy
 class ES6Spider(scrapy.Spider):
     name = 'ES6'
     allowed_domains = ['es6.ruanyifeng.com']
-    start_urls = ['http://es6.ruanyifeng.com/']
+    start_urls = ['http://es6.ruanyifeng.com']
 
     def parse(self, response):
         print(response)
